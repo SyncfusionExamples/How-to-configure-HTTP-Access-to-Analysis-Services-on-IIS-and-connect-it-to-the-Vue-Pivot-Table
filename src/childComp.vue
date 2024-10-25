4af48143beffd04509fe6a12a74d505323c45acf
AngelinFaithSheeba <angelinfaithsheeba.p@syncfusion.com>
2024-10-25 15:43:29 +0530

    917032: Fixed dependabot issue by updating the entire project.

---
+++ src/childComp.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="childComp"> {{ child }} </div>
+</template>
+
+<script lang="ts">
+  import Vue from 'vue';
+  export default Vue.extend({
+    inject: { child: { default: 'default'} } 
+  });
+</script>
